12e42a8069fb715db99f17259757d9d2dd6eff8d
jiangwei1 <jiangwei1@kylinos.com>
2021-07-27 17:55:27 +0800

    flaskenv文件

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+"""
+# coding:utf-8
+@Time    : 2021/07/27
+@Author  : jiangwei
+@mail    : qq804022023@gmail.com
+@File    : __init__.py.py
+@Desc    : __init__.py
+@Software: PyCharm
+"""
---
+++ app/models.py
@@ -0,0 +1,9 @@
+"""
+# coding:utf-8
+@Time    : 2021/07/27
+@Author  : jiangwei
+@mail    : qq804022023@gmail.com
+@File    : models.py
+@Desc    : models
+@Software: PyCharm
+"""
